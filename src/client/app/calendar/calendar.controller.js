a8723614d56714242496b1e03bf00bae1e421807
Taylor Handy <thandy1212@gmail.com>
2017-03-24 11:12:44 -0400

    Organized code into a little bit better function formatting as well as adding short comments for explaining functionality of calendar controller functions

--- src/client/app/calendar/calendar.controller.js
+++ src/client/app/calendar/calendar.controller.js
@@ -34,7 +34,7 @@
         vm.monthDays = 31;
         vm.count = 0;
         vm.currentYear = 2017;
-        vm.startDay = new Date(vm.currentYear,vm.currentMonth,1).getDay(); 
+        vm.startDay = new Date(vm.currentYear,vm.currentMonth,1).getDay();
 
         vm.selectedID = 1;
         vm.setSelected = setSelected;
@@ -42,15 +42,22 @@
         vm.previous = previous;       //Changes the calendar Month to the previous
         vm.next = next;               //Changes calendar month to next month
         vm.getMaxDays = getMaxDays;   //Gets the max number of days for the current Month
-
+        vm.isInvalidDate = isInvalidDate;
+        vm.calculateDate = calculateDate;
 
         var thirtyDays = [3,5,8,10];
 
         console.log("Initial Selected ID: " + vm.selectedID);
 
+        // getMaxDays is a method that sets the max days for a month with respect to
+        // the value of the current month and the current year (In case of leap year)
         function getMaxDays(){
           console.log("Executing getMaxDays()");
           if (vm.currentMonth === 1) {
+            if (isLeapYear(vm.currentYear)){
+              vm.monthDays = 29;
+              return;
+            }
             vm.monthDays = 28;
             return;
            }
@@ -71,7 +78,9 @@
 
         }
 
-
+        // previous is a method for controlling the back button on the UI to
+        // it simply subtracts 1 from the current month and sets it to 11 if the current month
+        // is 0. It also adjusts the startDay to correspond with the correct day of the week
          function previous(){
            console.log("curentMonth Before previous() = " + vm.currentMonth);
            if (vm.currentMonth === 0){
@@ -85,7 +94,7 @@
            console.log("currentMonth After previous() = " + vm.currentMonth);
          }
 
-
+         // next is essentially the same as previous method but increments instead of decrementing
          function next(){
            console.log("curentMonth Before next() = " + vm.currentMonth);
            if (vm.currentMonth === 11) {
@@ -99,11 +108,33 @@
            console.log("currentMonth After next() = " + vm.currentMonth);
          }
 
+         // setSelected is a simple setter for determing which day in UI is selected
          function setSelected(id){
            console.log("You clicked me");
 
            vm.selectedID = id;
            console.log("selectedID is " + vm.selectedID);
+         }
+
+         // isInvalidDate takes the weekNumber and dayNumber and checks to make sure the day is within the valid range for the current month
+         function isInvalidDate(dateEntry){
+           return dateEntry <= 0 || dateEntry > vm.monthDays;
+         }
+
+         // calculateDate simply calculates the date value based on the given weeekNum and dayNum
+         function calculateDate(weekNum, dayNum){
+           return ((weekNum - 1) * 7 + dayNum) - vm.startDay;
+         }
+
+         // isLeapYear checks if the passed in year is a leap year (E.g. February has 29 days instead of 28)
+         function isLeapYear(year){
+           if (year % 400 === 0){
+             return true;
+           }
+           if (year % 100 === 0){
+             return false;
+           }
+           return year % 4 === 0;
          }
 
     }
