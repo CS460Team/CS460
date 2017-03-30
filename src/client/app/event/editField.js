e6c47796cd002d684fff77fd1c3a90265c785b2a
caseyhult <caseyhult@gmail.com>
2017-03-30 14:38:21 -0400

    added delete functionality to events

---
+++ src/client/app/event/editField.js
@@ -0,0 +1,36 @@
+function EditFieldController($scope, $element, $attrs) {
+  var ctrl = this;
+  ctrl.editMode = false;
+
+  ctrl.handleModeChange = function() {
+    if (ctrl.editMode) {
+      ctrl.onUpdate({value: ctrl.fieldValue});
+      ctrl.fieldValueCopy = ctrl.fieldValue;
+    }
+    ctrl.editMode = !ctrl.editMode;
+  };
+
+  ctrl.reset = function() {
+    ctrl.fieldValue = ctrl.fieldValueCopy;
+  };
+
+  ctrl.$onInit = function() {
+    // Make a copy of the initial value to be able to reset it later
+    ctrl.fieldValueCopy = ctrl.fieldValue;
+
+    // Set a default fieldType
+    if (!ctrl.fieldType) {
+      ctrl.fieldType = 'text';
+    }
+  };
+}
+
+angular.module('app.event').component('editField', {
+  templateUrl: 'app/event/editField.html',
+  controller: EditFieldController,
+  bindings: {
+    fieldValue: '<',
+    fieldType: '@?',
+    onUpdate: '&'
+  }
+});
---
+++ src/client/app/event/eventDetail.js
@@ -0,0 +1,21 @@
+function EventDetailController() {
+  var ctrl = this;
+
+  ctrl.delete = function() {
+    ctrl.onDelete({event: ctrl.event});
+  };
+
+  ctrl.update = function(prop, value) {
+    ctrl.onUpdate({event: ctrl.event, prop: prop, value: value});
+  };
+}
+
+angular.module('app.event').component('eventDetail', {
+  templateUrl: 'app/event/eventDetail.html',
+  controller: EventDetailController,
+  bindings: {
+    event: '<',
+    onDelete: '&',
+    onUpdate: '&'
+  }
+});
